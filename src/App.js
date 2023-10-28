b067a810d056d84211b47c408f5c06d2e602aa1a
Amit Kumar <thisiskumaramit@gmail.com>
2023-10-28 19:57:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Statics from "./Components/Statics";
+import TypingInput from "./Components/TypingInput";
+import Login from "./Components/User/Login";
+import Signup from "./Components/User/Signup";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    let user = localStorage.getItem("user");
+
+    return (
+        <div className="app">
+            <Header />
+            <Routes>
+                <Route path="/typing-speed-test" element={<TypingInput />} />
+                <Route path="/statics" element={<Statics />} />
+                {user} ? <Route path="/login" element={<Login />} /> :{" "}
+                <Route path="/signup" element={<Signup />} />
+            </Routes>
+            <Footer />
+        </div>
+    );
+};
 
 export default App;
